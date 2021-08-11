63aa96e6422a14c111bdaa2282c8fcf17288db25
Thiago Roberto <thiago.nemo4code@gmail.com>
2021-08-11 11:28:57 -0300

    first commit

---
+++ src/schema.ts
@@ -0,0 +1,21 @@
+import { makeSchema } from 'nexus'
+
+export const schema = makeSchema({
+  types: [],
+  outputs: {
+    schema: __dirname + '/../schema.graphql',
+    typegen: __dirname + '/generated/nexus.ts',
+  },
+  contextType: {
+    module: require.resolve('./context'),
+    export: 'Context',
+  },
+  sourceTypes: {
+    modules: [
+      {
+        module: '@prisma/client',
+        alias: 'prisma',
+      },
+    ],
+  },
+})
